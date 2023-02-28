659012d5a3d6e6a78818451cbb966f17fed1df5f
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-02-28 23:04:22 +0200

    task-1 correct

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const items = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}" >
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+  </div>`;
+  })
+  .join("");
+
+const galleryBox = document.querySelector(".gallery");
+galleryBox.innerHTML = items;
+
+galleryBox.addEventListener("click", onLinkClick);
+
+function onLinkClick(event) {
+  event.preventDefault();
+
+  const clickImg = event.target;
+
+  if (clickImg.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+          <img src="${clickImg.dataset.source}">
+      `);
+  instance.show();
+}
 
 console.log(galleryItems);
