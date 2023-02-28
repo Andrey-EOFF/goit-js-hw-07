f52e47d99f0fbe155a6993722f7f02729cbae4c8
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-03-01 00:21:25 +0200

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const items = galleryItems
+  .map((item) => {
+    return `
+    <a class="gallery__item" href="${item.original}" >
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>`;
+  })
+  .join("");
+
+const galleryBox = document.querySelector(".gallery");
+galleryBox.innerHTML = items;
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  caption: true,
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  swipeClose: true,
+  animationSpeed: 300,
+});
 
 console.log(galleryItems);
